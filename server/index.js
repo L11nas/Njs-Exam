1ccc90fb6045f1b11278f412e7103be1a7139029
L11nas <ulevicius.linas@gmail.com>
2023-06-27 19:06:28 +0300

    index

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql2');
